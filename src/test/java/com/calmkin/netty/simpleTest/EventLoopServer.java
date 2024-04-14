2ebf6dce732347294133bd282c126d392a6ac0bc
calmkin <490363906@qq.com>
2024-04-14 12:01:28 +0800

    netty入门-测试eventLoop执行简单IO操作

---
+++ src/test/java/com/calmkin/netty/simpleTest/EventLoopServer.java
@@ -0,0 +1,41 @@
+package com.calmkin.netty.simpleTest;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+@Slf4j
+public class EventLoopServer {
+    public static void main(String[] args) {
+        new ServerBootstrap()
+                .group(new NioEventLoopGroup())
+                .channel(NioServerSocketChannel.class)
+                .childHandler(
+                        new ChannelInitializer<NioSocketChannel>() {
+                            @Override
+                            protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
+                                nioSocketChannel.pipeline().addLast(new ChannelInboundHandlerAdapter(){
+                                        @Override                                       // 如果没有指定解码器。那么这里得到的是byteBuf
+                                    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                        ByteBuf buf = (ByteBuf) msg;
+                                        log.debug(buf.toString(StandardCharsets.UTF_8) );
+                                    }
+                                });
+                            }
+                        }
+                )
+                .bind(8080);
+
+
+
+    }
+}
