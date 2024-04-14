befe7e03f5eab04695f54f9dbfc296f5dafe8bfb
calmkin <490363906@qq.com>
2024-04-14 10:34:14 +0800

    netty入门-netty实现简易版客户端

--- src/test/java/com/calmkin/netty/helloworld/HelloServer.java
+++ src/test/java/com/calmkin/netty/helloworld/HelloServer.java
@@ -1,4 +1,4 @@
-package com.calmkin.netty;
+package com.calmkin.netty.helloworld;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelHandlerContext;
@@ -22,11 +22,14 @@ public class HelloServer {
                 // handler决定了 worker能执行哪些操作
                 .childHandler(
                         // ChannelInitializer也是个handler，它负责添加其他handler
+                        // ChannelInitializer会在连接建立后被调用，做一个初始化操作
                         new ChannelInitializer<NioSocketChannel>() {
                             @Override
                             protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
-                                nioSocketChannel.pipeline().addLast(new StringDecoder());   // 将ByteBuf转换为字符串
+                                // 将ByteBuf转换为字符串, 同时交给下一个处理器
+                                nioSocketChannel.pipeline().addLast(new StringDecoder());
                                 // 自定义handler
+                                // 多个handler按照添加的顺序依次执行
                                 nioSocketChannel.pipeline().addLast(new ChannelInboundHandlerAdapter() {
                                     @Override   // 读事件
                                     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
